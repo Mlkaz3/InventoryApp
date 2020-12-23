59d8f34929db65beb6500a74788e27524bfc46a4
Winnie Yap <winnieyap20@gmail.com>
2020-12-24 01:59:19 +0800

    navigation drawer working

--- app/src/main/java/com/example/inventoryapp/MainActivity.kt
+++ app/src/main/java/com/example/inventoryapp/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.example.inventoryapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
+    //late initialise variable
+    lateinit var toolbar: Toolbar
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navigationView: NavigationView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        toolbar = findViewById(R.id.toolbar)
+
+            setSupportActionBar(findViewById(R.id.toolbar))
+
+//        //setSupportActionBar(toolbar)
+//
+//        drawerLayout = findViewById(R.id.DrawerLayout)
+//        navigationView = findViewById(R.id.NavigationView)
+//
+//        val Toggle = ActionBarDrawerToggle(
+//            //this,drawerLayout,toolbar,0,0
+//            this,drawerLayout,0,0
+//        )
+//
+//        drawerLayout.addDrawerListener(Toggle)
+//        Toggle.syncState()
+//        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+//        when(item.itemId){
+//            R.id.Inventory ->{
+//                Toast.makeText(applicationContext,"Inventory is Clicked",Toast.LENGTH_SHORT).show()
+//            }
+//
+//            R.id.UpdateInventory ->{
+//                Toast.makeText(applicationContext,"Update Inventory is Clicked",Toast.LENGTH_SHORT).show()
+//            }
+//
+//            R.id.StockChecking ->{
+//                Toast.makeText(applicationContext,"Stock Checking is Clicked",Toast.LENGTH_SHORT).show()
+//            }
+//
+//            R.id.StockUsage -> {
+//                Toast.makeText(applicationContext,"Stock Usage is Clicked",Toast.LENGTH_SHORT).show()
+//            }
+//
+//            R.id.History ->{
+//                Toast.makeText(applicationContext,"History is Clicked",Toast.LENGTH_SHORT).show()
+//            }
+//
+//        }
+
+        return true
     }
 }
