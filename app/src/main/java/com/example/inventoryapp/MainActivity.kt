a999969205fa9b34481106ad34d96733190dea9b
Winnie Yap <winnieyap20@gmail.com>
2020-12-25 02:42:22 +0800

    Fragment Implementation Done. Next Step: Inventory Fragment with Recycler View

--- app/src/main/java/com/example/inventoryapp/MainActivity.kt
+++ app/src/main/java/com/example/inventoryapp/MainActivity.kt
@@ -1,3 +1,5 @@
+//fragment credit: https://www.youtube.com/watch?v=Y8mGxxRe3k4&t=410s
+
 package com.example.inventoryapp
 
 import android.os.Bundle
@@ -6,7 +8,9 @@ import android.widget.Toast
 import android.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.FragmentTransaction
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
@@ -14,6 +18,13 @@ class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelected
     lateinit var drawerLayout: DrawerLayout
     lateinit var navigationView: NavigationView
 
+    //late initialise fragment
+    lateinit var homepageFragment: HomepageFragment
+    lateinit var inventoryFragment: InventoryFragment
+    lateinit var historyFragment: HistoryFragment
+    lateinit var updateInventoryFragment: UpdateInventoryFragment
+    lateinit var stockCheckingFragment: StockCheckingFragment
+    lateinit var stockUsageFragment: StockUsageFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,31 +44,78 @@ class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelected
         drawerLayout.addDrawerListener(Toggle)
         Toggle.syncState()
         navigationView.setNavigationItemSelectedListener(this)
+
+        homepageFragment = HomepageFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.frame_layout,homepageFragment)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
     }
 
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             R.id.Inventory ->{
-                Toast.makeText(applicationContext,"Inventory is Clicked",Toast.LENGTH_SHORT).show()
+                //Toast.makeText(applicationContext,"Inventory is Clicked",Toast.LENGTH_SHORT).show()
+                inventoryFragment = InventoryFragment()
+                supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, inventoryFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
             }
 
             R.id.UpdateInventory ->{
-                Toast.makeText(applicationContext,"Update Inventory is Clicked",Toast.LENGTH_SHORT).show()
+                //Toast.makeText(applicationContext,"Update Inventory is Clicked",Toast.LENGTH_SHORT).show()
+                updateInventoryFragment = UpdateInventoryFragment()
+                supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout, updateInventoryFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
             }
 
             R.id.StockChecking ->{
-                Toast.makeText(applicationContext,"Stock Checking is Clicked",Toast.LENGTH_SHORT).show()
+                //Toast.makeText(applicationContext,"Stock Checking is Clicked",Toast.LENGTH_SHORT).show()
+                stockCheckingFragment = StockCheckingFragment()
+                supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout,stockCheckingFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
             }
 
             R.id.StockUsage -> {
-                Toast.makeText(applicationContext,"Stock Usage is Clicked",Toast.LENGTH_SHORT).show()
+                //Toast.makeText(applicationContext,"Stock Usage is Clicked",Toast.LENGTH_SHORT).show()
+                stockUsageFragment = StockUsageFragment()
+                supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout,stockUsageFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
             }
 
             R.id.History ->{
-                Toast.makeText(applicationContext,"History is Clicked",Toast.LENGTH_SHORT).show()
+                //Toast.makeText(applicationContext,"History is Clicked",Toast.LENGTH_SHORT).show()
+                historyFragment = HistoryFragment()
+                supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.frame_layout,historyFragment)
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
             }
         }
+        drawerLayout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        else{
+            super.onBackPressed()
+        }
     }
 }
